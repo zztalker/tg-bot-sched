64741e6db1f3443de671052abfb3db6c12b1bf01
Pavel Zaikin <zaikinpv@gmail.com>
2024-10-12 14:28:30 +0200

    fix update script (reflect requirements change)
    * add requirements.txt and start-bot.sh
    * add pip install to start-bot.sh

--- start-bot.sh
+++ start-bot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /root/bot-sked
 . .venv/bin/activate
+pip install -r requirements.txt
 . .env
 python sched_bot.py
--- update.fish
+++ update.fish
@@ -1,4 +1,6 @@
 #!/opt/homebrew/bin/fish
 scp migration.py ny:/root/bot-sked/
 scp sched_bot.py ny:/root/bot-sked/
+scp requirements.txt ny:/root/bot-sked/
+scp start-bot.sh ny:/root/bot-sked/
 ssh ny "systemctl restart bot-sked"
